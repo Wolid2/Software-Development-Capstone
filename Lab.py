61fe0e418e5696bc394ec12ffc0a494b461999bb
wolid <cnnfolesocodeeiospp'@>
2025-08-25 20:01:08 -0500

    Lab_1 program for name and birth month check; program for list of classes

---
+++ Lab.py
@@ -0,0 +1,12 @@
+# Asking a user for their names and store it in the variables
+name = input('what is your name? ')
+# Asking the user for the month of their birthday
+Months_of_your_Birthday = input('What month were you born in? ')
+# Printing a message with the user's name and birth month
+print('Hello ' + name + '!')
+
+# Checking if the user's birth month is January
+if Months_of_your_Birthday.lower() == 'january':
+  print('Happy birth month!')
+# printing the number of letters in the user's name
+print(f'There are {len(name)} letters in your name')
---
+++ Lab_1.py
@@ -0,0 +1,16 @@
+# asking a user how many class is taking 
+Class_names = input('How many classes are you taking this Semester? ')  
+
+# we creating an empty list to store the class names
+class_list = []
+
+
+# looping over to get the class names
+for i in range(int(Class_names)):
+    class_name = input('Enter the name of the class: ')
+    class_list.append(class_name)
+
+# printing each class name on a separate line
+print('The classes you are taking this semester are:')
+for class_name in class_list:
+    print(class_name) 
